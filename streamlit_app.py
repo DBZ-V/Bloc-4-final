fa12cfb8ea2802c0fcc31a9aa5dee9a1ced48232
DBZ-V <vick.debouzy.38@gmail.com>
2025-02-02 08:53:21 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -113,7 +113,7 @@ if st.button("📈 Prédire le nombre de vélos disponibles"):
     lat, lon = station_info["Latitude"], station_info["Longitude"]
 
     # 🔹 Préparer la requête pour FastAPI
-    api_url = "http://35.180.47.228:8000/predict/" # pour del=loyment local : bike-api
+    api_url = "http://13.39.86.86:8000/predict/" # pour del=loyment local : bike-api
     data = {
         "station": station_filter,
         "hour": full_datetime.hour,
